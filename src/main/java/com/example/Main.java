f64342f85d06c29c14aee30fc735abcfca7ef6b5
denu06 <denishmakwana6@gmail.com>
2018-08-22 23:37:47 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -52,6 +52,11 @@ public class Main {
   String index() {
     return "index";
   }
+  
+   @RequestMapping("/test")
+  String index() {
+    return "";
+  }
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
